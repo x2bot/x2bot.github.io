63ee33f88585e8cda8cf9769cc8e3ad469954bd1
x2bot <30500949+x2bot@users.noreply.github.com>
2023-05-18 23:59:22 +0600

    Add files via upload

---
+++ protect.js
@@ -0,0 +1,12 @@
+	
+	var password; 
+	
+	var cp=2020;
+	
+	
+	password=prompt('Enter Password',''); 
+	if (password==cp) 
+	alert('Correct Password ! Click OK to Enter !');
+	else window.location.reload(true);
+	
+	
